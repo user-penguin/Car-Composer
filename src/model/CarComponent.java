748b2d699107b5e518ce4b0c1ddcbb2ef53da981
user-penguin <kobzev96@gmail.com>
2019-04-04 06:07:25 +0700

    add component interface

---
+++ src/model/CarComponent.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.ArrayList;
+
+public interface CarComponent {
+    boolean isItComposite();
+
+    void add(CarComponent component);
+
+    void remove(CarComponent component);
+
+    ArrayList<CarComponent> getChild();
+
+    String getName();
+
+    String getInfo();
+}
