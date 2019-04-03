19fd7db97de6eb89ab4fbae6a370d5c607bef341
user-penguin <kobzev96@gmail.com>
2019-04-04 06:32:12 +0700

    add view

--- src/model/Element.java
+++ src/model/Element.java
@@ -1,4 +1,35 @@
 package model;
 
-public class Element {
+import java.util.ArrayList;
+
+public class Element implements CarComponent {
+    @Override
+    public boolean isItComposite() {
+        return false;
+    }
+
+    @Override
+    public void add(CarComponent component) {
+
+    }
+
+    @Override
+    public void remove(CarComponent component) {
+
+    }
+
+    @Override
+    public ArrayList<CarComponent> getChild() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
 }
